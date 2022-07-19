3be64f7f142e14f53eb8c0fc21e6ec91fdde7c44
vitornapoleao <109608876+vitornapoleao@users.noreply.github.com>
2022-07-19 11:49:53 -0300

    Create exc004.lua

---
+++ exc004.lua
@@ -0,0 +1,10 @@
+print("digite os lados do triangulo em questão")
+io.write("digite lado A")
+ladoA = tonumber(io.read())
+io.write("digite o lado B")
+ladoB = tonumber(io.read())
+io.write("digite o lado C")
+ladoC = tonumber(io.read())
+perimetro = ladoA + ladoB + ladoC
+
+print("O perimetro do triangulo é:"..(perimetro))
