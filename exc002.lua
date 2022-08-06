065a71ee6a1900a2aad4665af7190e131a8cb5ef
vitornapoleao <109608876+vitornapoleao@users.noreply.github.com>
2022-08-05 22:51:00 -0300

    Update exc002.lua

--- exc002.lua
+++ exc002.lua
@@ -1,4 +1,4 @@
 io.write("Digite o lado do quadrado em questão:")
-    lado = tonumber(io.read())
-    print("Area ="..(lado * lado))
-    print("Perimetro ="..(lado * 4))
+lado = tonumber(io.read())
+print("Area ="..(lado * lado))
+print("Perimetro ="..(lado * 4))
