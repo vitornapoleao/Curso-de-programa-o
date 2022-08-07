f7374da46a4c4c63598b48b8f6c8b63106e3f3b0
vitornapoleao <109608876+vitornapoleao@users.noreply.github.com>
2022-08-07 01:29:32 -0300

    Update exc016.lua

--- exc016.lua
+++ exc016.lua
@@ -1,7 +1,11 @@
 io.write("digite um numero de 3 digitos:")
 numero = tonumber(io.read())
+
 d1 = numero % 10
 resto = math.floor(numero / 10)
+
 d2 = math.floor(resto % 10)
+
 d3 = math.floor(resto / 10)
- print(d1..d2..d3)
+
+print(d1..d2..d3)
