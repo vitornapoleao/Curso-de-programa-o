3e5b40c3c8d94e5bf11891abd3fe1c4b1cdcee5b
vitornapoleao <109608876+vitornapoleao@users.noreply.github.com>
2022-08-07 01:25:46 -0300

    Update exc008.lua

--- exc008.lua
+++ exc008.lua
@@ -1,7 +1,7 @@
 io.write("Digite o Valor em Fahrenheit:")
-fah = tonumber(io.read())
-celso = 5/9 * (fah - 32)
+fahrenheit= tonumber(io.read())
+celsius = 5/9 * (fahrenheit - 32)
 
 --C = 5/9 (F – 32)
 
-print("A temperatura em Celsius é igual a:"..(celso) )
+print("A temperatura em Celsius é igual a:"..(celsius) )
