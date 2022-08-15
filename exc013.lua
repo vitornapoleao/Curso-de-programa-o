f2c728c24b72c7568d24ab44878116d556d43c26
vitornapoleao <109608876+vitornapoleao@users.noreply.github.com>
2022-08-15 12:38:12 -0300

    Update exc013.lua

--- exc013.lua
+++ exc013.lua
@@ -4,6 +4,6 @@ io.write("digite a resistencia de R2")
 r2 = tonumber(io.read())
 io.write("digite a resistencia de r3")
 r3 = tonumber(io.read())
-ra = (r2 * r3) / (r2 + r3)
-rt = r1 + ra 
+ra = (r1 * r2) / (r1 + r2)
+rt = r3 + ra 
 print("a resistencia aproximada é"..(rt))
