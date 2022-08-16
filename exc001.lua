f5a2f5536daa8c87b93b4e5bb438fc7d7ff609ea
vitornapoleao <109608876+vitornapoleao@users.noreply.github.com>
2022-08-16 10:05:50 -0300

    Update exc001.lua

--- exc001.lua
+++ exc001.lua
@@ -1,8 +1,8 @@
 io.write("digite a altura do  retangulo")
-    altura = tonumber(io.read())
+altura = tonumber(io.read())
 io.write("digite o comprimento do retangulo:")
-    comprimento = tonumber(io.read())
-    area = comprimento * altura
-    perimetro = (comprimento * 2) + (altura * 2)
+comprimento = tonumber(io.read())
+area = comprimento * altura
+perimetro = (comprimento * 2) + (altura * 2)
 print(" A area é:"..(area))
 print(" O perimetro é "..(perimetro))
