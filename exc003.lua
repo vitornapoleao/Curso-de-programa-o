90bea90851f526eebca88e84964e8356478d1072
vitornapoleao <109608876+vitornapoleao@users.noreply.github.com>
2022-08-16 10:11:19 -0300

    Update exc003.lua

--- exc003.lua
+++ exc003.lua
@@ -1,8 +1,8 @@
 io.write("digite o raio:")
 raio = tonumber(io.read())
-xis = raio * raio
+raio_ao_quadrado = raio * raio
 
-pi2 = math.pi + math.pi
+pi_soma = math.pi + math.pi
 
-print("area igual a:"..(math.pi * xis))
-print("perimetro igual a:"..(pi2 * raio))
+print("area igual a:"..(math.pi * raio_ao_quadrado))
+print("perimetro igual a:"..(pi_soma * raio))
