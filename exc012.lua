cd601f58f017908ef37089d1bbeb8f4aca46652b
vitornapoleao <109608876+vitornapoleao@users.noreply.github.com>
2022-08-18 11:12:08 -0300

    Update exc012.lua

--- exc012.lua
+++ exc012.lua
@@ -1,5 +1,6 @@
 io.write("Digite a velociade em m/s:")
-velocidade_ms = tonumber(io.read())
-velocidade_kmh = velo_ms * 3.6
+velo_ms = tonumber(io.read())
+velo_kmh = velo_ms * 3.6
 
-print("A velocidade em Km/h é:"..(velocidade_kmh))
+print("A velocidade em Km/h é:"..(velo_kmh))
+
